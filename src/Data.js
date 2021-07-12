820abb025d331070568f46cd9cd5d6f3456de11e
DheerajSharma1011 <83813386+DheerajSharma1011@users.noreply.github.com>
2021-07-12 14:09:47 +0530

    LINKDIN

--- src/Data.js
+++ src/Data.js
@@ -1,33 +1,38 @@
-const slides = [
+const Data = [
 {
-   <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQE6ES80K3TmDw/company-logo_200_200/0/1594059882721?e=1634169600&v=beta&t=u6rz2DSAMcNSnO8EpdwWgLLUf5WZ8XZELFMjhMUVqWE" alt="profile loading"/>     
-   <h4>InVision</h4>
-   <p>Internet - 137K followers</p>
+    imgcr :"https://media-exp1.licdn.com/dms/image/C4D0BAQE6ES80K3TmDw/company-logo_100_100/0/1594059882721?e=1634169600&v=beta&t=x9tGOmXlipbH0a3mQgGi2mcV8rg1MpEvDFc6t1QCnPw",
+    title :"InVision",
+   followers:"Internet - 137K followers",
 },
 {
-   <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQFViIk8WWB7Eg/company-logo_200_200/0/1620738909153?e=1634169600&v=beta&t=lVzXDIOFV6Vu_js-SjgX1S1nQxkPlPbnSJ9qwxhFNms" alt="profile loading"/>    
-   <h4>Sketch</h4>
-   <p>Design - 33K followers</p>
+   imgscr :"https://media-exp1.licdn.com/dms/image/C4D0BAQFViIk8WWB7Eg/company-logo_200_200/0/1620738909153?e=1634169600&v=beta&t=lVzXDIOFV6Vu_js-SjgX1S1nQxkPlPbnSJ9qwxhFNms",    
+   title :  "Sketch -",
+   followers:"Design  - 33K followers",
 },
 {
-   <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQEZikrvnSHslA/company-logo_200_200/0/1546638528235?e=1634169600&v=beta&t=WOLWv6zYVcsYs4gzO6UP0DicBq4r8aMpGKYt_ejVTfo" alt="profile loading"/>     
-   <h4>Dribbble</h4>
-   <p>Design - 162K followers</p>
+   imgscr :"https://media-exp1.licdn.com/dms/image/C4E0BAQEZikrvnSHslA/company-logo_200_200/0/1546638528235?e=1634169600&v=beta&t=WOLWv6zYVcsYs4gzO6UP0DicBq4r8aMpGKYt_ejVTfo",     
+   title : "Dribbble",
+   followers: "Design - 162K followers",
 },
 {
-   <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQGOly_nkJ188g/company-logo_200_200/0/1519922105393?e=1634169600&v=beta&t=ORrbYxCnogmUFSjQCaLdR-D2tVzXKcV6qqOlue373Q4" alt="profile loading"/>     
-   <h4>Behance</h4>
-   <p>Internet - 67K followers</p>
+   imgscr :"https://media-exp1.licdn.com/dms/image/C4D0BAQGOly_nkJ188g/company-logo_200_200/0/1519922105393?e=1634169600&v=beta&t=ORrbYxCnogmUFSjQCaLdR-D2tVzXKcV6qqOlue373Q4",     
+   title : "Behance",
+   followers: "Internet - 67K followers",
 },
 {  
-   <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQFc2a9mdK6xmA/company-logo_200_200/0/1519911138730?e=1634169600&v=beta&t=BqVkRd9Oztq2gvMwz5LV233ZFymHrhFfGScVadtN9ZY" alt="profile loading"/>
-   <h4>Overlap Studio</h4>
-   <p>Information Technology & Services - 105K followers</p>
+   imgscr :"https://media-exp1.licdn.com/dms/image/C4E0BAQFc2a9mdK6xmA/company-logo_200_200/0/1519911138730?e=1634169600&v=beta&t=BqVkRd9Oztq2gvMwz5LV233ZFymHrhFfGScVadtN9ZY",
+   title : "Overlap",
+   followers: "Information Technology & Services - 105K followers",
 },
 {
-   <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQEv0xgEe3MJ2w/company-logo_200_200/0/1602698792035?e=1634169600&v=beta&t=YAbzczu22Tqn6-gn-DKfUXm9HSkAID_z_1iRBZxwpWk" alt="profile loading"/>  
-   <h4>Medium</h4>
-   <p>Online Media - 101K followers</p>
+   imgscr : "https://media-exp1.licdn.com/dms/image/C4D0BAQEv0xgEe3MJ2w/company-logo_200_200/0/1602698792035?e=1634169600&v=beta&t=YAbzczu22Tqn6-gn-DKfUXm9HSkAID_z_1iRBZxwpWk",
+   title : "Medium",
+   followers:"Online Media - 101K followers",
+},
+{
+   imgscr:"https://media-exp1.licdn.com/dms/image/C560BAQG-22OtXJPGpA/company-logo_100_100/0/1568751879393?e=1634169600&v=beta&t=_ZWfXIY4OAw51CwC7vQgmvJ1zjk0EdJ59eedbN4Vzko",
+   title :"Slack  __",
+   followers : "Computer Software 583k followers",
 },
 ];
-  export default slides;
+  export default Data;
--- src/ViewedPage.js
+++ src/ViewedPage.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./ViewedPage.css";
+import Data from "./Data"
  const ViewedPage = () =>{
  return(
       <div className="mainViewedPage">
        <div className="innerViewedPage">
+       
          <h3>Pages people also viewed</h3>
-                
+        { Data.map((val)=>{
+          return (
+                 <div className="ViewedPageusers">
+                   <img src={val.imgscr} alt ="profile loading"/>
+                   <h4>{val.title}</h4>
+                   <p>{val.followers}</p>
+                  </div>
+          )
+         })
+          }
+         
       </div>
      </div>
  	)
